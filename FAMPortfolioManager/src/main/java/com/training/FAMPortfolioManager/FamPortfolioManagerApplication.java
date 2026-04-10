5efb395ddf3b22f86537c9a4b0f6b544c7c8c0ba
Ajin Benny <ajinbenny2001@hotmail.com>
2026-04-10 13:50:05 +0000

    Adding Basic spring initaliser Folder

---
+++ FAMPortfolioManager/src/main/java/com/training/FAMPortfolioManager/FamPortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.training.FAMPortfolioManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamPortfolioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamPortfolioManagerApplication.class, args);
+	}
+
+}
