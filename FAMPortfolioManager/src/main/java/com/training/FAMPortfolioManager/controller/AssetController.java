4ea1d76392bbcda8708917eeecaf5d259e81ed7b
Max Pullen <maxojpullen@gmail.com>
2026-04-10 15:02:53 +0000

    import statements for AssetController

--- FAMPortfolioManager/src/main/java/com/training/FAMPortfolioManager/controller/AssetController.java
+++ FAMPortfolioManager/src/main/java/com/training/FAMPortfolioManager/controller/AssetController.java
@@ -1,5 +1,16 @@
 package com.training.FAMPortfolioManager.controller;
 
+import com.training.FAMPortfolioManager.repository.AssetRepository;
+import com.training.FAMPortfolioManager.model.Asset;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 public class AssetController {
 
 }
